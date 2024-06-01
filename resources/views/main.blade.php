16cde13494218da293a4fa2ac742b1fb19f46b0a
Chqrety <fikriaverus23@gmail.com>
2024-06-01 23:19:53 +0700

    feat: install Tailwind

---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="h-screen flex flex-col justify-center items-center gap-10 bg-blue-300">
+        <span class="text-3xl font-bold">Welcome Cooders</span>
+        <span>have a nice day :&rpar;</span>
+    </div>
+</body>
+
+</html>
